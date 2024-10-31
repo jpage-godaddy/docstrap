55a83aae8d67a155bec7af1e73637795240a8b80
batje <reinier@mountbatten.nl>
2024-10-31 14:18:25 +0100

    Updated dependencies and patches to make them work

--- Gruntfile.js
+++ Gruntfile.js
@@ -253,6 +253,7 @@ module.exports = function ( grunt ) {
 			_.each( list.themes, function ( entry ) {
 
 				toRun.push( "swatch" + entry.name );
+				console.log(entry.name);
 				grunt.registerTask( "swatch" + entry.name, _.partial( applyTheme, grunt, entry ) );
 
 				var key = "template/static/styles/site." + entry.name.toLowerCase() + ".css";
@@ -331,13 +332,23 @@ function applyTheme( grunt, definition ) {
 				if ( err ) {return cb( err );}
 				var fullPath = path.join( __dirname, "styles/bootswatch.less" );
 				fs.writeFile( fullPath, swatch.replace( "http://", webProtocol ), cb );
+				fs.readFile(fullPath, 'utf8', function (err,data) {
+					if (err) {
+					  return console.log(err);
+					}
+					var result = data.replace(/\@import\ url/g, '@import (css) url');
+				  console.log("tralala");
+					fs.writeFile(fullPath, result, 'utf8', function (err) {
+					   if (err) return console.log(err);
+					});
+				  });
 			} );
 		},
 		function ( cb ) {
 			getBootSwatchComponent( definition.lessVariables, function ( err, swatch ) {
 				if ( err ) {return cb( err );}
 				var fullPath = path.join( __dirname, "styles/variables.less" );
-				fs.writeFile( fullPath, swatch.replace( "http://", webProtocol ), cb );
+				fs.writeFile( fullPath, swatch.replace( "http://", webProtocol ), cb );				  
 			} );
 		}
 	], done );
@@ -354,7 +365,7 @@ function applyTheme( grunt, definition ) {
  * @private
  */
 function getBootSwatchList( done ) {
-	request('http://api.bootswatch.com/3/', function(error, response, body) {
+	request('http://bootswatch.com/api/3.json', function(error, response, body) {
 		if (error) {
 			return done(error);
 		}
